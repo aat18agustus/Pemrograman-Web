21d59993cf652ee7f05bdadd8ac0e4f2a95b26e1
Rijal <aat17agustus@gmail.com>
2021-05-01 14:36:38 +0700

    coba

---
+++ Tugas7PHP/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculated</title>
+</head>
+
+<body style="background-color: #95a5a6; text-align: center; font-size: 30px;">
+    <h1 style="font-size: 50px;">Calculated</h1>
+    <hr>
+    <?php
+    $String = "Hello world!";
+    $x = 5;
+    $y = 10.5;
+    echo "Ini adalah Variabel string<br>";
+    echo  $String, "<br>";
+    echo "Ini adalah Variabel A<br>";
+    echo  $x, "<br>";
+    echo "Ini adalah Variabel B<br>";
+    echo  $y, "<br>";
+    echo "Ini adalah Variabel A ditambah B<br>";
+    echo  $x + $y, "<br>";
+    echo "Ini adalah Variabel A dikurangi B<br>";
+    echo  $x - $y, "<br>";
+    echo "Ini adalah Variabel A dikali B<br>";
+    echo  $x * $y, "<br>";
+    echo "Ini adalah Variabel A dibagi B<br>";
+    echo  $x / $y, "<br>";
+    ?>
+</body>
+
+</html>
